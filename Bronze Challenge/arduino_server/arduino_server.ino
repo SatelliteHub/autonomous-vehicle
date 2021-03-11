ea0ae3f78f29986e0759be303ac13ed3f9cd913b
Mael Bonnec <bonnecm@tcd.ie>
2021-03-11 09:22:18 +0000

    Additional Message for the User added / Debugging

--- Bronze Challenge/arduino_server/arduino_server.ino
+++ Bronze Challenge/arduino_server/arduino_server.ino
@@ -48,13 +48,9 @@ int stop_all() { // STOP BUGGY
   analogWrite(RM2, 0);
 }
 
-int obstacle_seen; //For Message when Obstacle is seen
-
-// OPTIONAL MESSAGE VARIABLE
-int last_connected;
-int go_button_state;
-
-
+int obstacle_seen; // For Message when Obstacle is seen
+int last_connected; // For Message for Last Connection
+int go_button_state; // For Message when Go But. is H/L
 
 void setup() {
   Serial.begin(9600);
@@ -70,122 +66,85 @@ void setup() {
   pinMode(UST, OUTPUT);
   pinMode(USE, INPUT);
 
-
-
-
-
   WiFi.begin(ssid, pass);
   IPAddress ip = WiFi.localIP();
-  Serial.print("IP Adress:");
+  Serial.print("IP Address:");
   Serial.println(ip);
   server.begin();
-
-
 }
 
 void loop() {
   WiFiClient client = server.available();
   if (client.connected()) {
+    if (last_connected != 1) {
+      client.write("Client Connected\n");
+      last_connected = 1;
+    }
 
 
-    /* OPTIONAL: TELL USER WHEN GO BUTTON IS PRESSED
-      USING IF STATEMENT */
-    client.write ("Client Connected");
-    client.stop();
-
-    char c = client.read(); // Read char from Processing
-
+    char c = client.read();
     // 'g' for GO command and 's' for STOP command
     if (c == 'g') {
       while (c != 's') {
+        if (go_button_state != 1) {
+          client.write("Go Button Pressed\n");
+          go_button_state = 1;
+        }
 
-        client.write("Go Button Pressed");
-        move_forward();
-
-        /* OPTIONAL: TELL USER WHEN GO BUTTON IS PRESSED
-          USING IF STATEMENT */
-
-
-
+        // Ultrasonic
         int left = digitalRead(LEYE);
         int right = digitalRead(REYE);
 
-
-
-
-
-        // INSERT HERE THE ULTRASONIC CODE
         digitalWrite(UST, LOW);
         delayMicroseconds(2);
         digitalWrite(UST, HIGH);
         delayMicroseconds(10);
         digitalWrite(UST, LOW);
 
-        long duration = pulseIn(USE, HIGH); //Count when USE is high and stops when USE is low
+        float duration = pulseIn(USE, HIGH);
+        // Count when USE is HIGH and stop when USE is low
 
         int distance = duration / 58;
-
 
         // Detects when obstacle is further 10 cm dist.
         if (distance > 10) {
 
-
-
-
-
-
-          // INSERT HERE THE LINE FOLLOWING CODE
-          if (!left && !right) {
-            move_forward();
-          }
-
           if (left && right) {
             stop_all();
           }
-
+          if (!left && !right) {
+            move_forward();
+            obstacle_seen = 0;
+          }
           if (!left && right) {
             right_turn();
+            //obstacle_seen = 0;
           }
-
           if (left && !right) {
             left_turn();
+            //obstacle_seen = 0;
           }
-
-
-
-
-
-
-
         }
         // Detects when obstacle is within 10 cm dist.
         else if (distance < 10) {
           if (obstacle_seen != 1) {
-
-            client.write("stopping for obstacle at 10cm distance");
-
-            // TELEMETRY:
-            // PRINT TO PROCESSING CONSOLE THE MESSAGE
-            // (MESSAGE EXAMPLE ON BLACKBOARD > BRZE CH)
-
-
-            // CHANGE THE VARIABLE VALUE TO 1
-
-
+            client.write("Stopping for Obstacle at 10cm Distance\n");
+            obstacle_seen = 1;
           }
           stop_all();
         }
         c = client.read();
       }
+      client.write("Stop Button Pressed\n");
       stop_all();
-
-      // IF OPTIONAL CODE ABOVE IS USED RESET VARIABLE HERE
-
+      go_button_state = 0;
+      last_connected = 1;
     }
     else if (c == 's') {
-      client.write("Stop Button Pressed");
+      client.write("Stop Button Pressed\n");
       stop_all();
-      // IF OPTIONAL CODE ABOVE IS USED RESET VARIABLE HERE
+      go_button_state = 0;
+      last_connected = 1;
     }
   }
 }
