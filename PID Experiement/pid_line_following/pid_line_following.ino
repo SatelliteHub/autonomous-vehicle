edb77d3ee9a8f049e66d277dcb67ae2e14bc955e
Mael Bonnec <bonnecm@tcd.ie>
2021-03-22 09:55:06 +0000

    Added file for the PID Line Following Experiment

---
+++ PID Experiement/pid_line_following/pid_line_following.ino
@@ -0,0 +1,12 @@
+// TEMPLATE NOT READY YET
+// pid_us MUST BE WORKING BEFORE TACKLING LINE FOLLOWING
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
