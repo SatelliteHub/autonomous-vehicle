d1110fc9d87731c802e05aae77fa8037edab967e
Mael Bonnec <bonnecm@tcd.ie>
2021-03-27 09:53:08 +0000

    Code Restructure / Kp value changed / Debugging

--- Silver Challenge/arduino_server/arduino_server.ino
+++ Silver Challenge/arduino_server/arduino_server.ino
@@ -6,14 +6,13 @@ char ssid[] = " "; // DO NOT UPLOAD SSID ON GITLAB
 char pass[] = " "; // DO NOT UPLOAD PASSWORD ON GITLAB
 WiFiServer server(5200);
 
-
 // Define Variables we'll be connecting to
 double setpoint; // Desired Value
 double input; // US Sensor
 double output; // Motor
 
-// Insert Here PID Parameters (More info in the Lectures/Need testing)
-double Kp = 50, Ki = 5, Kd = 1;
+// PID Parameters
+double Kp = 20, Ki = 5, Kd = 1;
 
 // Specify the links and initial tuning parameters
 PID myPID(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
@@ -33,21 +32,21 @@ const int UST = 19;
 const int USE = 10;
 
 
-int right_turn() { // TURN RIGHT
+int right_turn() { // LEFT WHEEL FORWARD
   analogWrite(LM1, 255 - output);
   analogWrite(LM2, 0);
   analogWrite(RM1, 0);
   analogWrite(RM2, 0);
 }
 
-int left_turn() { // TURN LEFT
+int left_turn() { // RIGHT WHEEL FORWARD
   analogWrite(LM1, 0);
   analogWrite(LM2, 0);
   analogWrite(RM1, 255 - output);
   analogWrite(RM2, 0);
 }
 
-int move_forward() { // MOVE STRAIGHT
+int move_forward() { // ALL WHEEL FORWARD
   analogWrite(LM1, 255 - output);
   analogWrite(LM2, 0);
   analogWrite(RM1, 255 - output);
@@ -61,16 +60,17 @@ int stop_all() { // STOP BUGGY
   analogWrite(RM2, 0);
 }
 
-int last_distance;
-int obstacle_seen; // For Message when Obstacle is seen
-int last_connected; // For Message for Last Connection
-int go_button_state; // For Message when Go But. is H/L
+int distance; // Distance between the Obstacle and Vehicle
+int last_distance; // Last Recorded Distance
+int last_connected; // Message for Last Connection
+int go_button_state; // Message when Go But. is H/L
 
 void setup() {
   Serial.begin(9600);
 
-  setpoint = 12; // Insert here Distance to activate PID
+  setpoint = 15; // Distance to activate PID
   myPID.SetMode(AUTOMATIC); // Turn PID On
+  myPID.SetTunings(Kp, Ki, Kd); /// Adjust PID Values
 
   pinMode(LEYE, INPUT);
   pinMode(REYE, INPUT);
@@ -94,18 +94,16 @@ void loop() {
   WiFiClient client = server.available();
   if (client.connected()) {
     if (last_connected != 1) {
-      //Serial.println("Client Connected");
-      client.write("Client Connected\n");
+      //client.write("Client Connected\n");
       last_connected = 1;
     }
     char c = client.read();
-
+    
     // 'g' for GO command and 's' for STOP command
     if (c == 'g') {
       while (c != 's') {
         if (go_button_state != 1) {
-          //Serial.println("GO");
-          client.write("GO\n");
+          //client.write("GO");
           go_button_state = 1;
         }
 
@@ -122,84 +120,57 @@ void loop() {
         float duration = pulseIn(USE, HIGH);
         // Count when USE is HIGH and stop when USE is low
 
-        int distance = duration / 58;
-  
-         input = distance;
-         myPID.Compute(); // Compute PID
-  
-        // Print the corresponding Output HERE 
-        // Serial.println(255 - output);
+        distance = duration / 58;
 
-        
-        // Detects when obstacle is further 10 cm dist.
-        if (distance > 10) {
+        input = distance;
+        myPID.Compute(); // Compute PID
 
-          if (left && right) {
-            stop_all();
-            if (obstacle_seen != 0) {
-              client.write("No Obstacle\n");
-              obstacle_seen = 0;
-              // Assign "distance" to "last_distance"
-              last_distance = distance; 
-              delay(100);
-            }
-          }
-          if (!left && !right) {
-            move_forward();
-            if (obstacle_seen != 0) {
-              client.write("No Obstacle\n");
-              obstacle_seen = 0;
-              // Assign "distance" to "last_distance"
-              last_distance = distance; 
-              delay(100);
-            }
-
-          }
-          if (!left && right) {
-            right_turn();
-            if (obstacle_seen != 0) {
-              client.write("No Obstacle\n");
-              obstacle_seen = 0;
-              // Assign "distance" to "last_distance"
-              last_distance = distance; 
-              delay(100);
-            }
-          }
-          if (left && !right) {
-            left_turn();
-            if (obstacle_seen != 0) {
-              client.write("No Obstacle\n");
-              obstacle_seen = 0;
-              // Assign "distance" to "last_distance"
-              last_distance = distance; 
-              delay(100);
-            }
-          }
-        }
-        // Detects when obstacle is within 10 cm dist.
-        else if (distance <= 10) {
-          if (obstacle_seen != 1) {
-            //Serial.println("Stopping for Obstacle at 10cm Distance");
-            client.write("Stopping for Obstacle at 10cm Distance\n");
-            obstacle_seen = 1;
-            // Assign "distance" to "last_distance"
-            last_distance = distance; 
+        if (left && right) {
+          stop_all();
+          if (last_distance != distance) {
+            client.write(distance);
+            last_distance = distance;
+            Serial.println(last_distance);
             delay(100);
           }
-          stop_all();
+        }
+        if (!left && !right) {
+          move_forward();
+          if (last_distance != distance) {
+            client.write(distance);
+            last_distance = distance;
+            delay(100);
+          }
+        }
+
+        if (!left && right) {
+          right_turn();
+          if (last_distance != distance) {
+            client.write(distance);
+            last_distance = distance;
+            delay(100);
+          }
+        }
+        if (left && !right) {
+          left_turn();
+          if (last_distance != distance) {
+            client.write(distance);
+            last_distance = distance;
+            delay(100);
+          }
         }
         c = client.read();
       }
-      //Serial.println("STOP");
-      client.write("STOP\n");
       stop_all();
+      distance = 0;
+      client.write(distance);
       go_button_state = 0;
       last_connected = 1;
     }
     else if (c == 's') {
-      //Serial.println("STOP");
-      client.write("STOP\n");
       stop_all();
+      distance = 0;
+      client.write(distance);
       go_button_state = 0;
       last_connected = 1;
     }
